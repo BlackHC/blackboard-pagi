a7145fe4339bcb9be7f40848604f74575ac16c3d
Andreas Kirsch <blackhc@gmail.com>
2022-12-15 22:43:07 +0000

    Update author

--- blackboard_pagi/__init__.py
+++ blackboard_pagi/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Blackboard PAGI."""
 
-__author__ = """Andreas Kirsch"""
+__author__ = """Andreas Kirsch (Daedalus Lab Ltd)"""
 __email__ = 'blackhc@gmail.com'
 __version__ = '0.1.0'
