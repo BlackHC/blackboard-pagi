e5520c8de62d6bcc671c1ed1cfb302217e36fa66
blackhc <blackhc@gmail.com>
2023-06-02 15:27:56 +0100

    Ignore exceptions in the endpoint integration

--- blackboard_pagi/tools/trace_viewer/endpoint_integration.py
+++ blackboard_pagi/tools/trace_viewer/endpoint_integration.py
@@ -73,7 +73,5 @@ def trace_viewer_send_trace_builder(trace_builder: 'TraceBuilder', force: bool =
     payload = trace.dict()
     try:
         requests.post(url, json=payload, timeout=0.05)
-    except requests.exceptions.ConnectionError:
-        # If the connection fails, we don't want to delete the trace updates object
-        # so that the trace will be sent when the connection is restored
+    finally:
         pass
