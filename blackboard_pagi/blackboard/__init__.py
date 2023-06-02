67f152855ae862f71f14ab455f4a7e18c50a03c4
blackhc <blackhc@gmail.com>
2023-06-02 14:21:00 +0100

    Add copyright notices

--- blackboard_pagi/blackboard/__init__.py
+++ blackboard_pagi/blackboard/__init__.py
@@ -12,6 +12,22 @@ This file implements the Blackboard class, which is a wrapper around a git repos
 Blackboard class provides methods for reading and writing to the Blackboard, as well as methods for committing changes
 to the Blackboard.
 """
+#  Blackboard-PAGI - LLM Proto-AGI using the Blackboard Pattern
+#  Copyright (c) 2023. Andreas Kirsch
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pathlib
 import shutil
 from dataclasses import dataclass
--- blackboard_pagi/utils/tracer/handlers/trace_viewer.py
+++ blackboard_pagi/utils/tracer/handlers/trace_viewer.py
@@ -1,3 +1,19 @@
+#  Blackboard-PAGI - LLM Proto-AGI using the Blackboard Pattern
+#  Copyright (c) 2023. Andreas Kirsch
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from blackboard_pagi.tools.trace_viewer.endpoint_integration import trace_viewer_send_trace_builder
 from blackboard_pagi.utils.tracer.trace_builder import TraceBuilder, TraceBuilderEventHandler
 
